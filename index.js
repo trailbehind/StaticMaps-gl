310c94de0c6ecea2b6d025a9f36066dbba744c11
Jesse Crocker <jesse@gaiagps.com>
2020-02-22 19:07:15 -0700

    Implement route that includes bounds

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mapPool = getMap.getMapPool();
 
 //TODO: load background options
 
-function handleRequest(req, res, width, height, background, format = "png") {
+function handleRequest(req, res, width, height, background, extent, format = "png") {
   const imageFormat = imageUtils.parseImageFormat(format);
 
   var start = Date.now();
@@ -24,15 +24,23 @@ function handleRequest(req, res, width, height, background, format = "png") {
       map.useCount++;
 
       //TODO: set style
-      //TODO: calulate extent
       //TODO: add overlay
-
+      var overlayData;
+      if (extent == undefined) {
+        if (overlayData == undefined) {
+          return res.status(400).send("Request must include bounds, or post geojson data.");
+        }
+        //TODO: calulate extent from overlay data
+      }
+      //TODO: calculate zoom
+      var zoom = 10;
       const options = {
-        center: [45, -111],
+        center: [(extent[0] + extent[2]) / 2.0, (extent[1] + extent[3]) / 2.0],
         width: width,
         height: height,
-        zoom: 0
+        zoom: zoom
       };
+      debug("rendering map with options " + JSON.stringify(options));
 
       start = Date.now();
       map.render(options, (err, data) => {
@@ -51,20 +59,56 @@ function handleRequest(req, res, width, height, background, format = "png") {
     });
 }
 
-const app = express();
-const port = 3000;
-app.listen(port, () => console.log(`StaticMaps-gl listening on port ${port}!`));
-//TODO: change this to post
-app.get("/:width(\\d+)/:height(\\d+)/:background.:format", function(req, res) {
-  debug(req.params);
+function handleRequestWithBounds(req, res) {
+  debug("got request " + JSON.stringify(req.params));
+  const boundsString = req.params.bounds;
+  const bounds = boundsString.split(",").map(i => parseFloat(i));
+  const maxLat = 85;
+  const maxLon = 180;
+  if (bounds.length != 4) {
+    return res.status(400).send("Bounds must have 4 values.");
+  } else if (
+    Math.abs(bounds[0]) > maxLon ||
+    Math.abs(bounds[1]) > maxLat ||
+    Math.abs(bounds[2]) > maxLon ||
+    Math.abs(bounds[3]) > maxLat
+  ) {
+    return res.status(400).send("bounds out of range.");
+  } else if (bounds[0] > bounds[2] || bounds[1] > bounds[3]) {
+    return res.status(400).send("invalid bounds.");
+  }
   handleRequest(
     req,
     res,
     parseInt(req.params.width),
     parseInt(req.params.height),
     req.params.background,
+    bounds,
+    req.params.format
+  );
+}
+
+const app = express();
+const port = 3000;
+app.listen(port, () => console.log(`StaticMaps-gl listening on port ${port}!`));
+app.post("/:width(\\d+)/:height(\\d+)/:background.:format", function(req, res) {
+  debug("got request " + JSON.stringify(req.params));
+  handleRequest(
+    req,
+    res,
+    parseInt(req.params.width),
+    parseInt(req.params.height),
+    req.params.background,
+    undefined,
     req.params.format
   );
 });
 
-//TODO: add route to handle bbox
+app
+  .route("/:bounds/:width(\\d+)/:height(\\d+)/:background.:format")
+  .get(function(req, res) {
+    handleRequestWithBounds(req, res);
+  })
+  .post(function(req, res) {
+    handleRequestWithBounds(req, res);
+  });
